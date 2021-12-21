f93808cc2714422c0dc4a75b5705755ca9f45e21
Peter Sooley <psoots@gmail.com>
2021-12-20 21:13:18 -0600

    better fold for day01

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,3 +1,22 @@
+#[derive(Default)]
+struct FoldOne {
+    prev: u32, // 0 default causes extra check
+    count: u32,
+}
+
+impl FoldOne {
+    fn fold(f: Self, x: u32) -> Self {
+        Self {
+            prev: x,
+            count: if f.prev != 0 && x > f.prev {
+                f.count + 1
+            } else {
+                f.count
+            },
+        }
+    }
+}
+
 fn main() {
     let input = include_str!("../input.txt");
     let nums = input
@@ -5,16 +24,7 @@ fn main() {
         .filter(|s| !s.is_empty())
         .map(|x| x.parse::<u32>().unwrap());
 
-    let mut bumps = 0;
-    let mut prev = None;
-    for i in nums {
-        if let Some(p) = prev {
-            if i > p {
-                bumps += 1;
-            }
-        }
-        prev = Some(i);
-    }
+    let bumps = nums.fold(FoldOne::default(), FoldOne::fold);
 
-    println!("{}", bumps);
+    println!("{}", bumps.count);
 }
