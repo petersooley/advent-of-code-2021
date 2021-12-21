8775ed9936a4e47bfb5f0c495df608192e0b88c8
Peter Sooley <psoots@gmail.com>
2021-12-20 21:32:44 -0600

    done with day01

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -17,6 +17,57 @@ impl FoldOne {
     }
 }
 
+//           oldest, old, newest
+struct Triad(u32, u32, u32);
+
+impl Triad {
+    fn sum(&self) -> u32 {
+        self.0 + self.1 + self.2
+    }
+    fn shift(&self, next: u32) -> Self {
+        Self(self.1, self.2, next)
+    }
+}
+
+// we'd be in trouble if there are less than 3 items in the list
+enum FoldThree {
+    Init,
+    First(u32),
+    Second(u32, u32),
+    Ready { prev: Triad, count: u32 },
+}
+
+impl FoldThree {
+    fn fold(f: Self, next: u32) -> Self {
+        match f {
+            FoldThree::Init => FoldThree::First(next),
+            FoldThree::First(p) => FoldThree::Second(p, next),
+            FoldThree::Second(oldest, old) => FoldThree::Ready {
+                prev: Triad(oldest, old, next),
+                count: 0,
+            },
+            FoldThree::Ready { prev, count } => {
+                let shifted = prev.shift(next);
+                let count = if prev.sum() < shifted.sum() {
+                    count + 1
+                } else {
+                    count
+                };
+                FoldThree::Ready {
+                    prev: shifted,
+                    count,
+                }
+            }
+        }
+    }
+    fn count(&self) -> u32 {
+        match *self {
+            Self::Ready { count, .. } => count,
+            _ => unreachable!(),
+        }
+    }
+}
+
 fn main() {
     let input = include_str!("../input.txt");
     let nums = input
@@ -24,7 +75,9 @@ fn main() {
         .filter(|s| !s.is_empty())
         .map(|x| x.parse::<u32>().unwrap());
 
-    let bumps = nums.fold(FoldOne::default(), FoldOne::fold);
+    let bumps1 = nums.clone().fold(FoldOne::default(), FoldOne::fold);
+    let bumps3 = nums.fold(FoldThree::Init, FoldThree::fold);
 
-    println!("{}", bumps.count);
+    println!("fold one {}", bumps1.count);
+    println!("fold three {}", bumps3.count());
 }
